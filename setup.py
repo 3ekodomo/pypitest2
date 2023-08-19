013fd737f4cd29e9a704097aecaabfaf27c971ac
3ekodomo <133636085+3ekodomo@users.noreply.github.com>
2023-08-19 14:43:08 +0530

    update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "pypitestdomo",
+    name="helloworld-example",
     version="0.1",
     packages=find_packages(),
+    long_description="A simple 'Hello, World!' package.",
+    long_description_content_type="text/plain",  # or "text/x-rst" for RST
 )
