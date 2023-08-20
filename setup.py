56b7b2efc735fc9f3b080bef2f02c0009ece1563
3ekodomo <133636085+3ekodomo@users.noreply.github.com>
2023-08-20 11:42:32 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="helloworld-peko",
+    name="helloworld-pekousadaunique",
     version="0.2",
     packages=find_packages(),
     long_description="A simple package.",
