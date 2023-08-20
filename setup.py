14e021b9dfeaa03836a85091efd0fcc436ae3831
3ekodomo <133636085+3ekodomo@users.noreply.github.com>
2023-08-20 14:39:21 +0530

    update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name="pekousadaunique",
+    version="0.2",
     packages=find_packages(),
-    long_description="A simple package.",
+    long_description="A simple package. use **from earth import helloworld",
     long_description_content_type="text/plain",  # or "text/x-rst" for RST
 )
