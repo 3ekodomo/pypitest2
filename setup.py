63fcf8026d3486867e2ccfbb02699762f61021a6
3ekodomo <133636085+3ekodomo@users.noreply.github.com>
2023-08-20 11:30:58 +0530

    update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="helloworld-example",
-    version="0.1",
+    version="0.2",
     packages=find_packages(),
     long_description="A simple package.",
     long_description_content_type="text/plain",  # or "text/x-rst" for RST
