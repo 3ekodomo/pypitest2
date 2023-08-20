7a3144526de35c6c9f8848490b224ebf987b396b
3ekodomo <133636085+3ekodomo@users.noreply.github.com>
2023-08-20 11:30:48 +0530

    update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name="helloworld-example",
     version="0.1",
     packages=find_packages(),
-    long_description="A simple 'Hello, World!' package.",
+    long_description="A simple package.",
     long_description_content_type="text/plain",  # or "text/x-rst" for RST
 )
