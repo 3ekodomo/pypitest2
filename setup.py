784c8b9b8ec33f9eb75c8dae50dcee39265c6144
3ekodomo <133636085+3ekodomo@users.noreply.github.com>
2023-08-20 14:04:13 +0530

    update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ from setuptools import setup, find_packages
 
 setup(
     name="pekousadaunique",
-    version="2",
     packages=find_packages(),
     long_description="A simple package.",
     long_description_content_type="text/plain",  # or "text/x-rst" for RST
