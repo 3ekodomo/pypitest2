4dbb464110ad42463d68648c85d2a6d7f8fcd2fe
3ekodomo <133636085+3ekodomo@users.noreply.github.com>
2023-08-20 14:40:46 +0530

    update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name="pekousadaunique",
     version="0.2",
     packages=find_packages(),
-    long_description="A simple package. use **from earth import helloworld",
+    long_description="A simple package. use **from earth import helloworld and helloworld.do_waky_stuff() .",
     long_description_content_type="text/plain",  # or "text/x-rst" for RST
 )
